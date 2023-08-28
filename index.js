c6ea6dd850147ab2033abdcbdc5439b21661da22
RuhatB <r.uhat@yahoo.com.tr>
2023-08-28 21:39:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,14 +3,14 @@
 // Başlangıç Challenge'ı
 
 /**Örnek Görev: İlkini Dön
- * 
+ *
  * Bu örnek sonradan gelecek olan görevleri nasıl çözeceğinizi size gösterecek.
- * 
+ *
  * Aşağdıaki Yüksek dereceden fonskiyonu(higher-order function) kullanarak aşağıdakileri yapınız
  *  1. Stringlerden oluşan bir array'i parametre olarak alın
- *  2. Bir string'i değişken olarak alan bir callback fonksiyonunu parametre olarak alın 
+ *  2. Bir string'i değişken olarak alan bir callback fonksiyonunu parametre olarak alın
  *  3. Array'in İLK elemanını değişken olarak alarak çalışacak olan callback fonksiyonunun sonucunu dönün
- * 
+ *
  * Aşağıdaki kodlar bu görevin nasıl yapılacağına örnek olacaktır
  * Bu fonskiyon 'asas' dönmeli(return)
 */
@@ -26,13 +26,17 @@ console.log('örnek görev:', ilkiniDon(['as','sa'],function(metin){return metin
 ///// M V P ///////
 
 /*Görev 1: macSkoru()
-  
+
   Aşağıdaki skor1 ve skor2 kodlarını inceleyiniz ve aşağıdaki soruları altına not alarak cevaplayın
-  
+
   1. skor1 ve skor2 arasındaki fark nedir?
-  
+
+  skor1 bir closure skor2 bir scope , skor1 iç fonksiyon skor2 dışardaki fonksiyon..
+
   2. Hangisi bir closure kullanmaktadır? Nasıl tarif edebilirsin? (yarınki derste öğreneceksin :) )
-  
+
+  skor1 clousure kullanmakta,
+
   3. Hangi durumda skor1 tercih edilebilir? Hangi durumda skor2 daha mantıklıdır?
 */
 
@@ -54,24 +58,36 @@ function skor2() {
 }
 
 
-/* Görev 2: takimSkoru() 
+/* Görev 2: takimSkoru()
 Aşağıdaki takimSkoru() fonksiyonununda aşağıdakileri yapınız:
   1. Bir çeyrekte bir takımın ürettiği skoru rastgele(random) elde eden bir sonuc dönünüz(return)
-  
+
   Ön Bilgi: Bir çeyrekte takımlar ortalama 10 ile 25 sayı üretebiliyor
   Örnek: takimSkoru çağrıldığında 10-25 arasında bir skor dönmeli
-  
+
 Not: Bu fonskiyon, aşağıdaki diğer görevler için de bir callback fonksiyonu olarak da kullanılacak
 */
 
-function takimSkoru(/*Kodunuzu buraya yazınız*/){
-    /*Kodunuzu buraya yazınız*/
+
+
+function takimSkoru() {
+  var minScore = 10;
+  var maxScore = 25;
+  var sonuc= Math.floor(Math.random() * (maxScore - minScore + 1)) + minScore;
+  return sonuc;
 }
 
+var ceyrek =  takimSkoru();
+console.log("Çeyrek Skoru: " + ceyrek ;
+
+
+    /*Kodunuzu buraya yazınız*/
 
 
 
-/* Görev 3: macSonucu() 
+
+
+/* Görev 3: macSonucu()
 Aşağıdaki macSonucu() fonksiyonununda aşağıdakileri yapınız:
   1. Görev 2'de oluşturduğunuz 'takimSkoru'nu callback fonskiyonunu olarak ilk parametrede alın
   2. Bir basketbol maçında oynanan çeyrek sayısını ikinci parametre olarak alın
@@ -84,11 +100,26 @@ Aşağıdaki macSonucu() fonksiyonununda aşağıdakileri yapınız:
   "EvSahibi": 92,
   "KonukTakim": 80
 }
-*/ 
+*/function takimSkoru(ceyrek, evSahibi, konukTakım) {
+  var minScore = 10;
+  var maxScore = 25;
 
-function macSonucu(/*Kodunuzu buraya yazınız*/){
-  /*Kodunuzu buraya yazınız*/
+  var evSahibi = Math.floor(Math.random() * (maxScore - minScore + 1)) + minScore;
+  var konukTakım = Math.floor(Math.random() * (maxScore - minScore + 1)) + minScore;
+
+
+
+  return {
+    ev: evSahibi,
+    konuk: konukTakım,
+  };
 }
+
+var quarterNumber = 1;
+var homeTeam = ev;
+var awayTeam =   konuk;
+
+
 
 
 
@@ -100,7 +131,7 @@ Aşağıdaki periyotSkoru() fonksiyonununda aşağıdakileri yapınız:
   1. Görev 2'de oluşturduğunuz 'takimSkoru'nu callback fonskiyonunu olarak ilk parametrede alın
   2. takimSkoru callback fonksiyonunu kullanarak, EvSahibi ve KonukTakim için bir skor üretin
   3. Bir object olarak EvSahibi ve KonukTakim skorunu dönün
-  
+
 Örneğin: periyotSkoru(takimSkoru) çalıştırınca aşağıdaki object'i dönmeli
 {
   "EvSahibi": 18,
@@ -109,13 +140,14 @@ Aşağıdaki periyotSkoru() fonksiyonununda aşağıdakileri yapınız:
   */
 
 
-function periyotSkoru(/*Kodunuzu buraya yazınız*/) {
+function periyotSkoru(takimSkoru(),/) {
+
   /*Kodunuzu buraya yazınız*/
 
 }
 
 
-/* Zorlayıcı Görev 5: skorTabelasi() 
+/* Zorlayıcı Görev 5: skorTabelasi()
 Aşağıdaki skorTabelasi() fonksiyonunu kullanarak aşağıdakileri yapınız:
   1. İlk parametre olarak Görev 4'te oluşturduğumuz 'periyotSkoru'nu bir değişken olarak almalı
   2. İkinci parametre olarak Gröev 2'de oluşturduğumuz 'takimSkoru'nu bir değişken olarak almalı
@@ -125,23 +157,23 @@ Aşağıdaki skorTabelasi() fonksiyonunu kullanarak aşağıdakileri yapınız:
   6. Maç bitince de final skoru yazmalı: "Maç Sonucu: Ev Sahibi 101 - Konuk Takım 98"
 
 MAÇ UZAMAZ ise skorTabelasi(periyotSkoru,takimSkoru,4)
-  
+
 [
-  "1. Periyot: Ev Sahibi 10 - Konuk Takım 21", 
+  "1. Periyot: Ev Sahibi 10 - Konuk Takım 21",
   "2. Periyot: Ev Sahibi 20 - Konuk Takım 13",
-  "3. Periyot: Ev Sahibi 13 - Konuk Takım 9", 
-  "4. Periyot: Ev Sahibi 18 - Konuk Takım 11", 
-  "Maç Sonucu: Ev Sahibi 61 - Konuk Takım 54"  
+  "3. Periyot: Ev Sahibi 13 - Konuk Takım 9",
+  "4. Periyot: Ev Sahibi 18 - Konuk Takım 11",
+  "Maç Sonucu: Ev Sahibi 61 - Konuk Takım 54"
 ]
 
 MAÇ UZAR ise skorTabelasi(periyotSkoru,takimSkoru,4)
 [
-  "1. Periyot: Ev Sahibi 10 - Konuk Takım 21", 
+  "1. Periyot: Ev Sahibi 10 - Konuk Takım 21",
   "2. Periyot: Ev Sahibi 20 - Konuk Takım 13",
-  "3. Periyot: Ev Sahibi 13 - Konuk Takım 9", 
+  "3. Periyot: Ev Sahibi 13 - Konuk Takım 9",
   "4. Periyot: Ev Sahibi 18 - Konuk Takım 18",
-  "1. Uzatma: Ev Sahibi 10 - Konuk Takım 6" 
-  "Maç Sonucu: Ev Sahibi 71 - Konuk Takım 67"  
+  "1. Uzatma: Ev Sahibi 10 - Konuk Takım 6"
+  "Maç Sonucu: Ev Sahibi 71 - Konuk Takım 67"
 ]
 ] */
 // NOTE: Bununla ilgili bir test yoktur. Eğer logladığınız sonuçlar yukarıdakine benziyor ise tmamlandı sayabilirsiniz.
